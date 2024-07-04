072d85b4b201eb157e6166617e7712ee2a10bb9e
Keidson Roby <keidsondesigner@gmail.com>
2024-07-04 01:19:12 -0400

    feat: Add dependencia Flyway para migração do banco de dados

---
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -0,0 +1,10 @@
+CREATE TABLE categoria (
+  codigo BIGSERIAL PRIMARY KEY,
+  nome VARCHAR(50) NOT NULL
+);
+
+INSERT INTO categoria (nome) values ('Lazer');
+INSERT INTO categoria (nome) values ('Alimentação');
+INSERT INTO categoria (nome) values ('Supermercado');
+INSERT INTO categoria (nome) values ('Farmácia');
+INSERT INTO categoria (nome) values ('Outros');
