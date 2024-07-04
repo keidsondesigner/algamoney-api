51b07449e3a58134a9515e3427a1114a8daa85c4
Keidson Roby <keidsondesigner@gmail.com>
2024-07-04 13:22:32 -0400

    feat: Implementado método buscarPorCodigo() que retorna 404 se n for encontrado

--- src/main/java/com/keidson/algamoney_api/controller/CategoriaController.java
+++ src/main/java/com/keidson/algamoney_api/controller/CategoriaController.java
@@ -10,10 +10,12 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -41,5 +43,13 @@ public class CategoriaController {
 
     response.setHeader("Location", uri.toASCIIString());
     return ResponseEntity.created(uri).body(categoriaSalva);
+  }
+
+  @GetMapping("/{codigo}")
+  public ResponseEntity<CategoriaModel> buscarPorCodigo(@PathVariable Long codigo) {
+    Optional<CategoriaModel> categoriaModelOptional = this.categoriaRepository.findById(codigo);
+    return categoriaModelOptional.isPresent() ? 
+      ResponseEntity.ok(categoriaModelOptional.get()) : 
+      ResponseEntity.notFound().build();
   }
 }
