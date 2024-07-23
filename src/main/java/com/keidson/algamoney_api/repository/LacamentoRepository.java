03ca2681b4e2d3d84134f784215037f61a2913ef
Keidson Roby <keidsondesigner@gmail.com>
2024-07-23 18:40:19 -0400

    feat: Criado LacamentoRepository

---
+++ src/main/java/com/keidson/algamoney_api/repository/LacamentoRepository.java
@@ -0,0 +1,8 @@
+package com.keidson.algamoney_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.keidson.algamoney_api.model.LancamentoModel;
+
+public interface LacamentoRepository extends JpaRepository<LancamentoModel, Long> {
+
+}
