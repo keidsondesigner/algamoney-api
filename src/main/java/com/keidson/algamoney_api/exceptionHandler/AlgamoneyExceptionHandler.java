347c98478ffbde497ec22a720b0cb41cf9baa541
Keidson Roby <keidsondesigner@gmail.com>
2024-07-23 19:26:18 -0400

    feat: Tratamento de exceção quando criar() Laçamento e enviar o código categoria que não existe  no banco

--- src/main/java/com/keidson/algamoney_api/exceptionHandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/keidson/algamoney_api/exceptionHandler/AlgamoneyExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -65,6 +66,17 @@ public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
     String mensagemDesenvolvedor = ex.toString();
     List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
     return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+  }
+
+  @ExceptionHandler({ DataIntegrityViolationException.class })
+  public ResponseEntity<Object> handleDataIntegrityViolationException(
+    DataIntegrityViolationException ex, 
+    WebRequest request
+  ) {
+    String mensagemUsuario = messageSource.getMessage("recurso.operacao-nao-permitida", null, LocaleContextHolder.getLocale());
+    String mensagemDesenvolvedor = ex.toString();
+    List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+    return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
   }
 
   private List<Erro> criarListaDeErros(BindingResult bindingResult) {
