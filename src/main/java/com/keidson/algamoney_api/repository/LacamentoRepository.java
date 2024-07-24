61aec3ae94cca9730192e0540a4d16c70934dbd6
Keidson Roby <keidsondesigner@gmail.com>
2024-07-24 18:11:39 -0400

    feat: Criado método findByDescricaoAndDataVencimento

--- src/main/java/com/keidson/algamoney_api/repository/LacamentoRepository.java
+++ src/main/java/com/keidson/algamoney_api/repository/LacamentoRepository.java
@@ -1,8 +1,27 @@
 package com.keidson.algamoney_api.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import com.keidson.algamoney_api.model.LancamentoModel;
 
 public interface LacamentoRepository extends JpaRepository<LancamentoModel, Long> {
+
+  @Query(
+    """    
+      SELECT l FROM LancamentoModel l WHERE
+      (:descricao IS NULL OR l.descricao LIKE %:descricao%) AND
+      (:dataVencimentoDe IS NULL OR l.dataVencimento >= :dataVencimentoDe) AND
+      (:dataVencimentoAte IS NULL OR l.dataVencimento <= :dataVencimentoAte)
+    """)
+  List<LancamentoModel> findByDescricaoAndDataVencimento(
+      @Param("descricao") String descricao,
+      @Param("dataVencimentoDe") LocalDate dataVencimentoDe,
+      @Param("dataVencimentoAte") LocalDate dataVencimentoAte
+  );
 
 }
