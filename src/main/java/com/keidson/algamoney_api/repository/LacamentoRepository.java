344636b8d6ad493f4b9ceaab964ab83e8711ba87
Keidson Roby <keidsondesigner@gmail.com>
2024-07-25 07:45:18 -0400

    chore: atualização Laçamentos Query findByDescricaoAndDataVencimento()

--- src/main/java/com/keidson/algamoney_api/repository/LacamentoRepository.java
+++ src/main/java/com/keidson/algamoney_api/repository/LacamentoRepository.java
@@ -11,17 +11,15 @@ import com.keidson.algamoney_api.model.LancamentoModel;
 
 public interface LacamentoRepository extends JpaRepository<LancamentoModel, Long> {
 
-  @Query(
-    """    
-      SELECT l FROM LancamentoModel l WHERE
-      (:descricao IS NULL OR l.descricao LIKE %:descricao%) AND
-      (:dataVencimentoDe IS NULL OR l.dataVencimento >= :dataVencimentoDe) AND
-      (:dataVencimentoAte IS NULL OR l.dataVencimento <= :dataVencimentoAte)
-    """)
+  @Query(""" 
+    SELECT l FROM LancamentoModel l 
+    WHERE (:descricao IS NULL OR l.descricao ILIKE %:descricao%)
+    AND (COALESCE(:dataVencimentoDe, l.dataVencimento) = l.dataVencimento OR l.dataVencimento >= :dataVencimentoDe)
+    AND (COALESCE(:dataVencimentoAte, l.dataVencimento) = l.dataVencimento OR l.dataVencimento <= :dataVencimentoAte)
+  """)
   List<LancamentoModel> findByDescricaoAndDataVencimento(
-      @Param("descricao") String descricao,
-      @Param("dataVencimentoDe") LocalDate dataVencimentoDe,
-      @Param("dataVencimentoAte") LocalDate dataVencimentoAte
-  );
+    @Param("descricao") String descricao,
+    @Param("dataVencimentoDe") LocalDate dataVencimentoDe,
+    @Param("dataVencimentoAte") LocalDate dataVencimentoAte);
 
 }
