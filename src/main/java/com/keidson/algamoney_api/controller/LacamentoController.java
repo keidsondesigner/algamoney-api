2cacf3ca16a5aff561a97e097246c427c11995ff
Keidson Roby <keidsondesigner@gmail.com>
2024-07-25 09:31:09 -0400

    feat: criado método remover Lançamento por Id

--- src/main/java/com/keidson/algamoney_api/controller/LacamentoController.java
+++ src/main/java/com/keidson/algamoney_api/controller/LacamentoController.java
@@ -11,6 +11,7 @@ import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.keidson.algamoney_api.event.RecursoCriadoEvent;
@@ -80,6 +82,12 @@ public class LacamentoController {
 
     publisher.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalvo.getCodigo()));
     return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalvo);
+  }
+
+  @DeleteMapping("/{codigo}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void remover(@PathVariable Long codigo) {
+    this.lancamentoService.remove(codigo);
   }
 
   @ExceptionHandler({ PessoaInexistenteOuInativaException.class })
--- src/main/java/com/keidson/algamoney_api/service/LancamentoService.java
+++ src/main/java/com/keidson/algamoney_api/service/LancamentoService.java
@@ -2,8 +2,11 @@ package com.keidson.algamoney_api.service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.keidson.algamoney_api.exceptionHandler.PessoaInexistenteOuInativaException;
 import com.keidson.algamoney_api.model.LancamentoModel;
@@ -36,6 +39,18 @@ public class LancamentoService {
 
   public List<LancamentoModel> filtrarLancamentos(String descricao, LocalDate dataVencimentoDe, LocalDate dataVencimentoAte) {
     return lacamentoRepository.findByDescricaoAndDataVencimento(descricao, dataVencimentoDe, dataVencimentoAte);
+  }
+
+  @Transactional
+  public void remove(Long codigo) {
+    // Buscar o lançamento pelo ID
+    Optional<LancamentoModel> lancamento = this.lacamentoRepository.findById(codigo);
+    // Verificar se o lançamento não foi encontrado lançar uma exceção
+    if (lancamento.isEmpty()) {
+        throw new EmptyResultDataAccessException(1);
+    }
+    // Se o lançamento foi encontrado, removê-lo
+    this.lacamentoRepository.deleteById(codigo);
   }
 
 }
