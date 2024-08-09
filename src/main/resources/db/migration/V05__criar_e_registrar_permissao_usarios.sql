0c632573909f3fbc68a1d11d3f0a789462444ba7
Keidson Roby <keidsondesigner@gmail.com>
2024-08-09 10:49:00 -0400

    feat: Flyway criado massa de dados tabela permissao

---
+++ src/main/resources/db/migration/V05__criar_e_registrar_permissao_usarios.sql
@@ -0,0 +1,39 @@
+-- Criação da tabela 'permissao'
+CREATE TABLE permissao (
+  codigo BIGINT PRIMARY KEY,
+  descricao VARCHAR(50) NOT NULL
+);
+
+-- Criação da tabela 'usuario_permissao'
+CREATE TABLE usuario_permissao (
+  codigo_usuario BIGINT NOT NULL,
+  codigo_permissao BIGINT NOT NULL,
+  PRIMARY KEY (codigo_usuario, codigo_permissao),
+  FOREIGN KEY (codigo_usuario) REFERENCES usuario(codigo),
+  FOREIGN KEY (codigo_permissao) REFERENCES permissao(codigo)
+);
+
+-- Inserções para tabela 'permissao'
+INSERT INTO permissao (codigo, descricao) VALUES (1, 'ROLE_CATEGORIA_CADASTRAR');
+INSERT INTO permissao (codigo, descricao) VALUES (2, 'ROLE_CATEGORIA_PESQUISAR');
+INSERT INTO permissao (codigo, descricao) VALUES (3, 'ROLE_PESSOA_CADASTRAR');
+INSERT INTO permissao (codigo, descricao) VALUES (4, 'ROLE_PESSOA_REMOVER');
+INSERT INTO permissao (codigo, descricao) VALUES (5, 'ROLE_PESSOA_PESQUISAR');
+INSERT INTO permissao (codigo, descricao) VALUES (6, 'ROLE_LANCAMENTO_CADASTRAR');
+INSERT INTO permissao (codigo, descricao) VALUES (7, 'ROLE_LANCAMENTO_REMOVER');
+INSERT INTO permissao (codigo, descricao) VALUES (8, 'ROLE_LANCAMENTO_PESQUISAR');
+
+-- Inserções para tabela 'usuario_permissao' (USUARIO-ADMIN-PERMISSAO)
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) VALUES (1, 1);
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) VALUES (1, 2);
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) VALUES (1, 3);
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) VALUES (1, 4);
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) VALUES (1, 5);
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) VALUES (1, 6);
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) VALUES (1, 7);
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) VALUES (1, 8);
+
+-- Inserções para tabela 'usuario_permissao' (USUARIO-NORMAL-PERMISSAO)
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) VALUES (2, 2);
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) VALUES (2, 5);
+INSERT INTO usuario_permissao (codigo_usuario, codigo_permissao) VALUES (2, 8);
