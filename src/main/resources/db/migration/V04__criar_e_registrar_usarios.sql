bc491800804ff03987ee4aefe8f0905b136eadf3
Keidson Roby <keidsondesigner@gmail.com>
2024-08-12 09:59:21 -0400

    feat: atualização flyway

--- src/main/resources/db/migration/V04__criar_e_registrar_usarios.sql
+++ src/main/resources/db/migration/V04__criar_e_registrar_usarios.sql
@@ -1,6 +1,6 @@
 -- Criação da tabela 'usuario'
 CREATE TABLE usuario (
-  codigo BIGINT PRIMARY KEY,
+  codigo SERIAL PRIMARY KEY,
   nome VARCHAR(50) NOT NULL,
   email VARCHAR(50) NOT NULL,
   senha VARCHAR(150) NOT NULL
--- src/main/resources/db/migration/V06__criar_e_registrar_sequencia_id_usuario.sql
+++
@@ -1,2 +0,0 @@
--- Ajusta a sequência do campo 'codigo' para o próximo valor disponível
--- SELECT setval('usuario_codigo_seq', (SELECT MAX(codigo) FROM usuario));
