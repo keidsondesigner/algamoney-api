ff2802d1911db6dfe8dd8f7c5ff38552e3cac600
Keidson Roby <keidsondesigner@gmail.com>
2024-10-01 23:22:05 -0400

    chore: atualização Cors

--- src/main/java/com/keidson/algamoney_api/cors/CorsConfig.java
+++ src/main/java/com/keidson/algamoney_api/cors/CorsConfig.java
@@ -10,9 +10,11 @@ public class CorsConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:4200", "https://algamoney-frontend-beige.vercel.app") 
-        .allowedOrigins("*")
-        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    .allowedOrigins("http://localhost:4200", "https://algamoney-frontend-beige.vercel.app") 
+        .allowedOrigins("*") 
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+        .allowedHeaders("*")
+        .allowCredentials(true);
   }
 
 }
